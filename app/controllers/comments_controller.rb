dba7c04b7d4a4ff382e4caf3b29e881644237b84
ynsture <ynsture@gmail.com>
2020-05-23 13:43:41 +0300

    end of project

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+    def index
+    end
+    def show
+    end
+    def new
+    end
+    def edit
+    end
+    def create
+        @article=Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+
+
+    end
+    def update
+    end
+    def destroy
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article)
+    end
+    private
+    def comment_params
+      params.require(:comment).permit(:author, :body)
+    end
+
+end
